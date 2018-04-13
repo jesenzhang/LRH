3ad78f581b5d1b12a04f36346f45c6fb24516e98
jesenzhang <jesen-zh@outlook.com>
2018-04-13 14:42:05 +0800

    init

---
+++ Assets/Scripts/Data/SystemDataProperty.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EnumProperty;
+
+/// <summary>
+/// 系统的属性
+/// </summary>
+public class SystemDataProperty : ScriptableObject
+{
+    public CardProperty[] AllCards;
+    public RoundProperty[] AllRounds;
+    public RoleProperty[] AllRoles;
+    public DeckProperty[] AllDecks;
+    public LevelData[] AllLevels;
+    public Card[] GameAllCards;
+}
---
+++ Assets/Scripts/UISystem/UI/UINotice.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using VFrame.UI;
+using UnityEngine.UI;
+
+public class UINotice : UIPage
+{
+    Text tips;
+
+    public UINotice() : base(UIType.PopUp, UIMode.DoNothing, UICollider.Normal)
+    {
+       
+    }
+
+    public override void Awake(GameObject go)
+    {
+        this.gameObject.transform.Find("content/btn_confim").GetComponent<Button>().onClick.AddListener(() =>
+        {
+            Hide();
+        });
+        tips = this.gameObject.transform.Find("content/Text").GetComponent<Text>();
+    }
+    public override void Active()
+    {
+        base.Active();
+        tips.text = (string)data;
+    }
+    public override void Refresh()
+    {
+        
+    }
+}
